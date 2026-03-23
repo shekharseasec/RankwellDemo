30265865c6ca642400c2bea788f4bacc5ccf3585
Seasec <seasec@.com>
2026-03-23 13:35:28 +0530

    Intial commit

---
+++ spring-backend/rankwelldemo/src/main/java/com/spring/rankwelldemo/RankwelldemoApplication.java
@@ -0,0 +1,13 @@
+package com.spring.rankwelldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RankwelldemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RankwelldemoApplication.class, args);
+	}
+
+}
---
+++ spring-backend/rankwelldemo/src/test/java/com/spring/rankwelldemo/RankwelldemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.rankwelldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RankwelldemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
